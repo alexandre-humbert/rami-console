f7b18684f77cdadb7e195dc5426cad476da99cbb
AurelienArbaretaz <50328631+AurelienArbaretaz@users.noreply.github.com>
2020-05-25 12:21:42 +0200

    Update Joueur.cpp

--- Rami/Rami/Joueur.cpp
+++ Rami/Rami/Joueur.cpp
@@ -6,7 +6,7 @@ Carte Joueur::choisirCarte(int i)
 
 	return main[i];
 }
-// Retourne une combinason de cartes séléectionnées par le joueur.
+// Retourne une combinason de cartes sÃ©lÃ©ectionnÃ©es par le joueur.
 Combinaison Joueur::nouvelleCombinaison()
 {
 	vector<int> ids;
@@ -31,7 +31,7 @@ Combinaison Joueur::nouvelleCombinaison()
 		return combi;
 
 }
-// Affiche les cartes que le joueur à en main.
+// Affiche les cartes que le joueur Ã  en main.
 void Joueur::afficherMain()
 {
 	for (int i = 0; i < (int)main.size(); i++)
@@ -42,7 +42,7 @@ void Joueur::afficherMain()
 	}
 }
 
-// Permet au joueur de piocher une carte de la pioche qui est renouvelée si besoin.
+// Permet au joueur de piocher une carte de la pioche qui est renouvelÃ©e si besoin.
 void Joueur::piocher()
 {
 	if (pioche_->getTaillePioche() != 0)
@@ -62,12 +62,12 @@ void Joueur::piocher()
 	}
 }
 
-// Permet au joueur de défausser une carte choisie.
+// Permet au joueur de dÃ©fausser une carte choisie.
 void Joueur::defausser() {
 	afficherMain();
 	int choix;
 	do {
-		cout << "Choisissez la carte à défausser : " << endl;
+		cout << "Choisissez la carte Ã  dÃ©fausser : " << endl;
 		cin >> choix;
 	} while (choix < 0 || choix>7);
 	choix--;
@@ -94,98 +94,6 @@ Joueur::Joueur(string nom, string id, Pioche *pioche)
 Joueur::~Joueur()
 {
 }
-#include "Joueur.h"
-#include "Carte.h"
-#include <iostream>
-Carte Joueur::choisirCarte(int i)
-{
 
-	return main[i];
-}
-
-// Retourne une combinason de cartes séléectionnées par le joueur.
-Combinaison Joueur::nouvelleCombinaison()
-{
-	vector<int> ids;
-	int id = 0;
-	do
-	{
-
-		afficherMain();
-		cout << "Choisir l'id d'une carte et -1 pour finir : " << endl;
-		cin >> id;
-		if (id = !- 1)
-		{
-			ids.push_back(id);
-		}
-	} while (id = !- 1);
-	vector<Carte> cartes;
-	for (int i = 0; i < ids.size(); i++)
-	{
-		cartes.push_back(main[i]);
-	}
-	Combinaison combi(cartes);
-	return combi;
-
-}
-
-// Affiche les cartes que le joueur à en main.
-void Joueur::afficherMain()
-{
-	for (int i = 0; i < (int)main.size(); i++)
-	{
-		std::cout << i + 1 << ".";
-		main[i].afficherCarte();
-		std::cout << " \n";
-	}
-}
-
-// Permet au joueur de piocher une carte de la pioche qui est renouvelée si besoin.
-void Joueur::piocher()
-{
-	if (pioche_->getTaillePioche() != 0)
-	{
-		Carte c = pioche_->tirerCarte();
-		cout << "Carte piochee : ";
-		c.afficherCarte();
-		main.push_back(c);
-	}
-	else {
-		if (pioche_->getTailleDefausse() != 0) {
-			pioche_->renouvelerPioche();
-		}
-		else {
-			cout << "Plus de carte" << endl;
-		}
-	}
-}
-
-// Permet au joueur de défausser une carte choisie.
-void Joueur::defausser() {
-	afficherMain();
-	int choix;
-	do {
-		cout << "Choisissez la carte à défausser : " << endl;
-		cin >> choix;
-	} while (choix < 0 || choix>7);
-	choix--;
-	pioche_->defausserCarte(main[choix]);
-	main.erase(main.begin() + choix);
-	nombreCarte_--;
-}
-
-// Constructeur de joueur
-Joueur::Joueur(string nom, string id, Pioche *pioche)
-{
-	pioche_ = pioche;
-	id_ = id;
-	nom_ = nom;
-
-	for (int i = 0; i < 7; i++)
-	{
-		main.push_back(pioche_->tirerCarte());
-	}
-	nombreCarte_ = 7;
-}
 
 
