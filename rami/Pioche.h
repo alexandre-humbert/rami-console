e792aca119bb14ed5a253b8b638fa7728ad1bacb
AurelienArbaretaz <50328631+AurelienArbaretaz@users.noreply.github.com>
2020-06-01 15:39:52 +0200

    Update Pioche.h

--- rami/Pioche.h
+++ rami/Pioche.h
@@ -1,17 +1,23 @@
 #pragma once
 #include <vector>
 #include "Carte.h"
+//Cette classe va gérer à la fois la pioche et la défausse du jeu.
 
+mélanger permet de mélanger les cartes de la pioche.\newline
+Lorsque la pioche est vide on peut utiliser la défausse pour la renouveler.\newline
+tirerCarte permet à un joueur de tirer une carte sur le dessus de la pioche.\newline
+\newline
+On peut prendre la carte de la déffausse avec prendreDeffausse.\newline
 class Pioche
 {
-	vector<Carte> cartes_;
-	int taillePioche_=53;
+	vector<Carte> cartes_;//On a comme attribut des vecteurs de Carte qui correspondent respectivement à la pioche et à la défausse.
+	int taillePioche_=53;//On a aussi leur taille actuelle.
 	vector<Carte>defausse_;
 	int tailleDefausse_ = 0;
 
 public:
-	Pioche(string name =" ");
-	int getTaillePioche();
+	Pioche(string name =" ");//Le constructeur de pioche va initialiser les cartes de la pioche et va les mélanger ou non si il s'agit d'une démo ou non.
+	int getTaillePioche();//On peut pour chaque attribut utiliser des set et des get.\newline
 	int getTailleDefausse();
 	vector<Carte> getPioche() { return cartes_; }
 	vector<Carte> getDefausse() { return defausse_; }
@@ -19,12 +25,12 @@ public:
 	void setTailleDefausse(int value) { tailleDefausse_ = value; }
 	void setPioche(vector<Carte> value) {cartes_=value; }
 	void setDefausse(vector<Carte> value) { defausse_=value; }
-	void melanger();
-	void renouvelerPioche();
-	Carte tirerCarte();
-	void afficherDefausse();
-	void defausserCarte(Carte c);
-	Carte prendreDefausse();
+	void melanger();//Permet de mélanger les cartes de la pioche.\newline
+	void renouvelerPioche();//Lorsque la pioche est vide on peut utiliser la défausse pour la renouveler.
+	Carte tirerCarte();//tirerCarte permet à un joueur de tirer une carte sur le dessus de la pioche.
+	void afficherDefausse();//Avec afficherDefausse on peut afficher la première carte de la défausse.
+	void defausserCarte(Carte c);//On peut deffausser une carte et ainsi l'ajouter à la défausse.
+	Carte prendreDefausse();//On peut prendre la carte de la déffausse avec prendreDeffausse.
 	~Pioche();
 };
 
