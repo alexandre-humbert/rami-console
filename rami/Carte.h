9a737f72c141562610e649a8af91f3af45fe65a5
AurelienArbaretaz <50328631+AurelienArbaretaz@users.noreply.github.com>
2020-06-01 15:35:31 +0200

    Update Carte.h

--- rami/Carte.h
+++ rami/Carte.h
@@ -2,18 +2,18 @@
 #include <string>
 
 using namespace std;
-
+//Cette classe va permettre de créer et manipuler chacune des cartes.
 class Carte
 {
-	string valeur_;
+	string valeur_;//Cette classe à deux attributs qui vont chacun indiquer une valeur et une couleur.
 	string couleur_;
 public:
 	Carte(string valeur="0",string couleur="0");
-	void afficherCarte();
-	bool operator < ( Carte& carte) ;
-	bool operator == ( Carte& carte) ;
-	int operator - (Carte& carte);
-	string getValeur();
+	void afficherCarte();//va afficher une carte.
+	bool operator < ( Carte& carte) ;//permet de comparer la valeur de deux cartes.
+	bool operator == ( Carte& carte) ;//permet de comparer la valeur de deux cartes.
+	int operator - (Carte& carte);//- on retourne la différence de deux valeurs de cartes en prenant en compte le Joker et l'as on l'utilisera pour verifier q'une suite est bonne.
+	string getValeur();//getValeur et getCouleur permettent d'obtenir la valeur des attributs.
 	string getCouleur();
 	~Carte();
 };
